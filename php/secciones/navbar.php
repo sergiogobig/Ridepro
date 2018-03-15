227d0c1123ebdc9a612307afe9f0381148a2920f
Michael <micaelsosa@fernandosor.edu.co>
2018-03-14 20:12:11 -0500

    Arreglo del navbaar
    Ahora el navbar nos lleva donde es

--- php/secciones/navbar.php
+++ php/secciones/navbar.php
@@ -1,25 +1,25 @@
 <div class="navbar-fixed">
   <nav class="navegacion nav-wrapper">
      <div class="nav-wrapper margennavegacion">
-        <a style="line-height: 135px;" href="#" class="brand-logo">
+        <a style="line-height: 135px;" href="index.php" class="brand-logo">
           <img style="width: 250px;" src="../imagenes/Logo 2.png">
         </a>
         <ul id="nav-mobile" class="right hide-on-med-and-down black-text navbar-ul">
            <li class="paralelogramo">
              <img class="navbar-item-img" src="../imagenes/RP.png" alt="">
-             <a class="black-text" href="#page-top">HOME</a>
+             <a class="black-text" href="index.php#page-top">HOME</a>
            </li>
            <li class="paralelogramo">
              <img class="navbar-item-img" src="../imagenes/RP.png" alt="">
-             <a class="black-text" href="#cursos">CURSOS</a>
+             <a class="black-text" href="index.php#cursos">CURSOS</a>
            </li>
            <li class="paralelogramo">
              <img class="navbar-item-img" src="../imagenes/RP.png" alt="">
-             <a class="black-text" href="#eventos">EVENTOS</a>
+             <a class="black-text" href="index.php#eventos">EVENTOS</a>
            </li>
            <li class="paralelogramo">
              <img class="navbar-item-img" src="../imagenes/RP.png" alt="">
-             <a class="black-text" href="#equipo">EQUIPO</a>
+             <a class="black-text" href="index.php#equipo">EQUIPO</a>
            </li>
         </ul>
      </div>

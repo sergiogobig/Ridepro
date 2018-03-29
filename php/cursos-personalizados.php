52d6df89ac89fe32f12816ccc6009ef8039a2bfd
Michael <micaelsosa@fernandosor.edu.co>
2018-03-29 17:42:17 -0500

    Revert "ESTRUCTURA PAGINA 2 TERMINADA"
    This reverts commit 2e552b1e735acd3e0844d53ee9619fc6840e4338.

--- php/cursos-personalizados.php
+++ php/cursos-personalizados.php
@@ -2,103 +2,53 @@
 <?php include("secciones/head.php"); ?>
 <!-- Aqui la barra de navegacion -->
 <?php include("secciones/navbar.php"); ?>
-<!-- Content -->
+
+ <!-- Content -->
+
+
+
 <?php include("secciones/cursos/seccion1.php"); ?>
 <?php include("secciones/cursos/seccion2.php"); ?>
-<section id="section4" style="position: relative; top: -7rem;">
-   <div class="row">
-     <img  class="cortada" src="../imagenes/background4.jpg" alt="">
-      <div class="col s6 offset-s6">
-         <div class="row tipos-cursos" style="top: 0px;">
-            <div class="col m12">
-               <div class="row titulos-cursos" >
-                  <div class="titulo-curso titulo-gris ubicacion1">curso</div>
-                  <p class="ubicacion1"><strong>maxi trail</strong></p>
-                  <div class="subtitulo-card ub-1">
-                     <div class="llanta-marca"></div>
-                     <ul>
-                        <li>
-                           <h5>5 horas practicas en la pista</h5>
-                        </li>
-                        <li>
-                           <h5>1 hora de masajes</h5>
-                        </li>
-                        <li>
-                           <h5>1 salda  a rumbear</h5>
-                        </li>
-                     </ul>
-                  </div>
-               </div>
-               <!-- Aqui los cuadros con la descripcion -->
-               <div class="row">
-                  <div class="col s12" style="padding-right: 20px;">
-                     <div class="card forma-curso" style="padding-top: 11rem; background: red;">
-                        <div class="card-content white-text">
-                           <p>I am a very simple card. I am good at containing small bits of information.
-                              I am convenient because I require little markup to use effectively.
-                           </p>
-                        </div>
-                        <div class="card-action">
-                           <ul class="ul-inline">
-                              <li><a href="#">Duracion: 2 dias</a></li>
-                              <li><a href="#">Precio: $310.000</a></li>
-                           </ul>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
-   <svg width="0" height="0">
-      <defs>
-         <clipPath id="myClip">
-            <polygon points="67 420,279 420,402 202,279 0,67 0,203 202"></polygon>
-            <polygon points="553 -1,663 199,553 420,373 420,487 203,373 0">
-            </polygon>
-         </clipPath>
-      </defs>
-   </svg>
-</section>
-<section id="section3">
+
+<section id="">
    <div class="container">
       <div class="row">
-         <div class="col m12 forma-curso" style="padding: 25px 10px 0px 32px; background-color: #757575;">
-            <div class="row" style="margin-bottom: 0px;">
-               <div class="col m2">
-                  <img width="110%" src="../imagenes/tools.png">
-               </div>
-               <div class="col m10">
-                  <p>
-                     <span class="titulo-curso titulo-rojo">Mejore sus</span>
-                     <span class="titulo-curso titulo-gris"> habilidades mecánicas</span>
-                  </p>
-                  <p>Bacon ipsum dolor amet ball tip spare ribs jowl porchetta fatback venison swine pork tri-tip. Biltong ground round rump pancetta drumstick. Beef ribs buffalo cupim landjaeger meatball shoulder pork chop ball tip alcatra bacon ground round shankle chuck. Corned beef pork salami porchetta rump pork chop spare ribs prosciutto shoulder swine pig. Leberkas capicola frankfurter, buffalo ribeye picanha fatback salami. Spare ribs jerky venison sausage shoulder landjaeger.</p>
-               </div>
+         <div class="col m12 forma-curso" style="padding: 25px 10px 0px 32px;">
+           <div class="row">
+             <div class="col m2">
+               <img width="110%" src="http://via.placeholder.com/150x180">  
+             </div>
+             <div class="col m10">
+              <p class="titulo-curso">Mejore sus<span> habilidades mecánicas</spam></p>
+              <p>Bacon ipsum dolor amet ball tip spare ribs jowl porchetta fatback venison swine pork tri-tip. Biltong ground round rump pancetta drumstick. Beef ribs buffalo cupim landjaeger meatball shoulder pork chop ball tip alcatra bacon ground round shankle chuck. Corned beef pork salami porchetta rump pork chop spare ribs prosciutto shoulder swine pig. Leberkas capicola frankfurter, buffalo ribeye picanha fatback salami. Spare ribs jerky venison sausage shoulder landjaeger.</p>
             </div>
-            <div class="row">
-               <div class="col m12">
-                  <p>Aprenderas:</p>
-                  <ol>
-                     <li>Bacon ipsum dolor amet ball tip spare ribs jowl porchetta fatback venison swsadsadasdine pork tri-</li>
-                     <li>Bacon ipsum dolor amet ball tip spare rdasdaibs jowl porchetta fatback venison swine pork tri-</li>
-                     <li>Bacon ipsum dolor amet ball tip spare ribs jowl porcdashetta fatback venison swine asdaspork tri-</li>
-                     <li>Bacon ipsum dolor amet ball tip spare rdasdasibs jowl pordaschetta fatback venison swine pork tri-</li>
-                     <li>Bacon ipsum dolor amet ball tip spaddre ribs jowl porchetta fatback venison swine psadasork tri-</li>
-                     <li>Bacon ipsum dolor amet ball tip spare ribs jowl porchetta fatback venison swinsadsdsadasdasdae pork tri-</li>
-                     <li>Bacon ipsum dolor amet ball tip spare ribs jowl porchetta fatback venison swine pork tri-</li>
-                     <li>Bacon ipsum dolor amet ball tip spare ribs jasdasowl porchetta fatback venison swine pork tri-</li>
-                  </ol>
-                  <p>Duracion: 3 horas    |  Duracion: 3 horas</p>
-               </div>
+           </div>
+           <div class="row">
+             <div class="col m12">
+              <p>Aprenderas:</p>
+              <ol>
+                <li>Bacon ipsum dolor amet ball tip spare ribs jowl porchetta fatback venison swsadsadasdine pork tri-</li>
+                <li>Bacon ipsum dolor amet ball tip spare rdasdaibs jowl porchetta fatback venison swine pork tri-</li>
+                <li>Bacon ipsum dolor amet ball tip spare ribs jowl porcdashetta fatback venison swine asdaspork tri-</li>
+                <li>Bacon ipsum dolor amet ball tip spare rdasdasibs jowl pordaschetta fatback venison swine pork tri-</li>
+                <li>Bacon ipsum dolor amet ball tip spaddre ribs jowl porchetta fatback venison swine psadasork tri-</li>
+                <li>Bacon ipsum dolor amet ball tip spare ribs jowl porchetta fatback venison swinsadsdsadasdasdae pork tri-</li>
+                <li>Bacon ipsum dolor amet ball tip spare ribs jowl porchetta fatback venison swine pork tri-</li>
+                <li>Bacon ipsum dolor amet ball tip spare ribs jasdasowl porchetta fatback venison swine pork tri-</li>
+              </ol>
+              <p>Duracion: 3 horas    |  Duracion: 3 horas</p>
             </div>
+           </div>
          </div>
       </div>
-   </div>
+    </div>
    </div>
 </section>
+
+
+
 <!-- * END Content -->
+
 <!-- Incluir Contacto -->
 <?php include("secciones/contacto.php"); ?>
 <!-- Aqui el footer -->

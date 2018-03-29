a6aaf3ebb4cd2afa852246e782c4a55b650e3b07
Michael <micaelsosa@fernandosor.edu.co>
2018-03-29 18:05:41 -0500

    Revert "Revert "ESTRUCTURA PAGINA 2 TERMINADA""
    This reverts commit 52d6df89ac89fe32f12816ccc6009ef8039a2bfd.

--- php/secciones/cursos/seccion1.php
+++ php/secciones/cursos/seccion1.php
@@ -1,22 +1,15 @@
 <section class="slider-intro" id="page-top" >
-
-
-         <div class="parallax-container slider-top2">
-            <div class="parallax">
-
-               <img class="imgenparallax" width="100%" src="../imagenes/portadacursoper2.jpg">
-            </div>
-            <div>
-              <img class="acomodarjustoper" src="../imagenes/cursopersonalizado3.png" alt="">
-            </div>
-         </div>
-
-
-
-      <div class="row container">
-         <div class="col m12 text-section textosparrafos">
-            <p>Nuestros formatos VIP Ride Pro COURSES, están diseñados para todos los niveles de manejo. Desde aquellos que jamás han tenido contacto con motos, hasta pilotos experimentados que desean mejorar y perfeccionar su técnica de manejo en cualquier tipo de terreno. Con nuestro equipo de trabajo y gran experiencia en formación a motociclistas, le garantizamos que obtendrá la mejor experiencia y cumplirá con sus objetivos propuestos.</p>
-         </div>
-      </div>
-
+   <div class="carousel carousel-slider center" data-indicators="true">
+     <div class="carousel-fixed-item col s12">
+       <div class="encabezado-pagina">
+       </div>
+     </div>
+     <div class="carousel-item foto1 white-text" href="#one!">
+     </div>
+   </div>
+   <div class="container row">
+     <div class="col m12 text-section">
+      <p>Nuestros formatos VIP Ride Pro COURSES, están diseñados para todos los niveles de manejo. Desde aquellos que jamás han tenido contacto con motos, hasta pilotos experimentados que desean mejorar y perfeccionar su técnica de manejo en cualquier tipo de terreno. Con nuestro equipo de trabajo y gran experiencia en formación a motociclistas, le garantizamos que obtendrá la mejor experiencia y cumplirá con sus objetivos propuestos.</p>
+     </div>
+   </div>
 </section>

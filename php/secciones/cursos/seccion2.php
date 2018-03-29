3eb51453ef8b5ae3dc3c0e86a68ec209792fa760
Michael <micaelsosa@fernandosor.edu.co>
2018-03-29 18:28:40 -0500

    Revert "Revert "Revert "Revert "ESTRUCTURA PAGINA 2 TERMINADA""""
    This reverts commit 36cc4355c6f544c6e56d573fb7d89fd01c65628a.

--- php/secciones/cursos/seccion2.php
+++ php/secciones/cursos/seccion2.php
@@ -1,105 +1,122 @@
 <section id="cursos-descripcion">
-
-      <div class="row">
-         <div class="col m12 center-align section-subheading">
-            <h5>Por medio de la capacitaciòn y el deporte, salvamos vidas <img src="../imagenes/RPN.png" alt=""></h5>
+   <div class="container row">
+       <div class="col m12 center-align section-subheading">
+          <h5>Por medio de la capacitaciòn y el deporte, salvamos vidas </h5>
+       </div>
+       <div class="campo-imagen">
+          <div class="llanta-marca"></div>
+       </div>
+   </div>
+   <div class="row tipos-cursos">
+      <div class="col m12">
+         <!-- Estos son solo los titulos -->
+         <div class="row titulos-cursos">
+            <!-- Titulo curso 1 -->
+            <div class="titulo-curso titulo-rojo ubicacion1">
+               vip ride pro course
+            </div>
+            <p class="ubicacion1"><strong>nivel 1</strong></p>
+            <div class="subtitulo-card ub-1">
+               <div class="llanta-marca"></div>
+               <ul>
+                  <li>
+                     <h5>5 horas practicas en la pista</h5>
+                  </li>
+                  <li>
+                     <h5>1 hora de masajes</h5>
+                  </li>
+                  <li>
+                     <h5>1 salda  a rumbear</h5>
+                  </li>
+               </ul>
+            </div>
+            <!-- Titulo curso 2 -->
+            <div class="titulo-curso titulo-rojo ubicacion2">
+               3 dias
+            </div>
+            <p class="ubicacion2"><strong>nivel 2</strong></p>
+            <div class="subtitulo-card ub-2">
+               <div class="llanta-marca"></div>
+               <ul>
+                  <li>
+                     <h5>5 horas practicas en la pista</h5>
+                  </li>
+                  <li>
+                     <h5>1 hora de masajes</h5>
+                  </li>
+                  <li>
+                     <h5>1 salda  a rumbear</h5>
+                  </li>
+               </ul>
+            </div>
+            <!-- Titulo curso 3 -->
+            <div class="titulo-curso  titulo-rojo ubicacion3">
+               7 dias
+            </div>
+            <p class="ubicacion3"><strong>nivel 3</strong></p>
+            <div class="subtitulo-card ub-3">
+               <div class="llanta-marca"></div>
+               <ul>
+                  <li>
+                     <h5>5 dias de manejo en pista</h5>
+                  </li>
+                  <li>
+                     <h5>1 hora de masajes</h5>
+                  </li>
+                  <li>
+                     <h5>1 salda  a rumbear</h5>
+                  </li>
+               </ul>
+            </div>
          </div>
-
+         <!-- Aqui los cuadros con la descripcion -->
+         <div class="row">
+            <div class="col s12 m4" style="padding-right: 20px;">
+               <div class="card forma-curso">
+                  <div class="card-content">
+                     <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.
+                     </p>
+                  </div>
+                  <div class="card-action">
+                     <ul class="ul-inline">
+                        <li><a href="#">Duracion: 2 dias</a></li>
+                        <li><a href="#">Precio: $310.000</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <div class="col s12 m4" style="padding-right: 20px;">
+               <div class="card forma-curso">
+                  <div class="card-content">
+                     <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.
+                     </p>
+                  </div>
+                  <div class="card-action">
+                     <ul class="ul-inline">
+                        <li><a href="#">Duracion: 2 dias</a></li>
+                        <li><a href="#">Precio: $310.000</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <div class="col s12 m4" style="padding-right: 20px;">
+               <div class="card forma-curso">
+                  <div class="card-content">
+                     <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.
+                     </p>
+                  </div>
+                  <div class="card-action">
+                     <ul class="ul-inline">
+                        <li><a href="#">Duracion: 2 dias</a></li>
+                        <li><a href="#">Precio: $310.000</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
       </div>
-
-
-      <div class="row" style=" padding-left: 5rem; padding-right: 5rem;">
-         <div class="col m12">
-            <!-- Estos son solo los titulos -->
-            <div class="row titulos-cursos">
-              <!-- Titulo curso 1 -->
-               <div class="titulo-curso ubicacion1">
-                  vip ride pro course
-               </div>
-               <p class="ubicacion1"><strong>nivel 1</strong></p>
-               <div class="subtitulo-card ub-1">
-                  <div class="llanta-marca"></div>
-                  <ul>
-                     <li><h5>5 horas practicas en la pista</h5></li>
-                     <li><h5>1 hora de masajes</h5></li>
-                     <li><h5>1 salda  a rumbear</h5></li>
-                  </ul>
-               </div>
-               <!-- Titulo curso 2 -->
-               <div class="titulo-curso ubicacion2">
-                  3 dias
-               </div>
-               <p class="ubicacion2"><strong>nivel 2</strong></p>
-               <div class="subtitulo-card ub-2">
-                  <div class="llanta-marca"></div>
-                  <ul>
-                     <li><h5>5 horas practicas en la pista</h5></li>
-                     <li><h5>1 hora de masajes</h5></li>
-                     <li><h5>1 salda  a rumbear</h5></li>
-                  </ul>
-               </div>
-               <!-- Titulo curso 3 -->
-               <div class="titulo-curso ubicacion3">
-                  7 dias
-               </div>
-               <p class="ubicacion3"><strong>nivel 3</strong></p>
-               <div class="subtitulo-card ub-3">
-                  <div class="llanta-marca"></div>
-                  <ul>
-                     <li><h5>5 dias de manejo en pista</h5></li>
-                     <li><h5>1 hora de masajes</h5></li>
-                     <li><h5>1 salda  a rumbear</h5></li>
-                  </ul>
-               </div>
-            </div>
-            <!-- Aqui los cuadros con la descripcion -->
-            <div class="row">
-               <div class="col s12 m4" style="padding-right: 20px;">
-                  <div class="card forma-curso">
-                     <div class="card-content">
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                           I am convenient because I require little markup to use effectively.
-                        </p>
-                     </div>
-                     <div class="card-action">
-                        <ul class="ul-inline">
-                           <li><a href="#">Duracion: 2 dias</a></li>
-                           <li><a href="#">Precio: $310.000</a></li>
-                        </ul>
-                     </div>
-                  </div>
-               </div>
-               <div class="col s12 m4" style="padding-right: 20px;">
-                  <div class="card forma-curso">
-                     <div class="card-content">
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                           I am convenient because I require little markup to use effectively.
-                        </p>
-                     </div>
-                     <div class="card-action">
-                        <ul class="ul-inline">
-                           <li><a href="#">Duracion: 2 dias</a></li>
-                           <li><a href="#">Precio: $310.000</a></li>
-                        </ul>
-                     </div>
-                  </div>
-               </div>
-               <div class="col s12 m4" style="padding-right: 20px;">
-                  <div class="card forma-curso">
-                     <div class="card-content">
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                           I am convenient because I require little markup to use effectively.
-                        </p>
-                     </div>
-                     <div class="card-action">
-                        <ul class="ul-inline">
-                           <li><a href="#">Duracion: 2 dias</a></li>
-                           <li><a href="#">Precio: $310.000</a></li>
-                        </ul>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
    </div>
 </section>

259277e305da500de9dc3902bdef35578c05f518
Michael <micaelsosa@fernandosor.edu.co>
2018-05-30 06:19:01 -0500

    MotofestIV
    cree estructura de motofest para que trabaje sobre esa

---
+++ php/motofestiv.php
@@ -0,0 +1,128 @@
+<!-- Aqui el head -->
+<?php include("secciones/head.php"); ?>
+<!-- Aqui la barra de navegacion -->
+<?php include("secciones/navbar.php"); ?>
+<!-- Content -->
+<div class="parallax-container slider-top2">
+
+   <div class="parallax">
+      <img class="imgenparallax" width="100%" src="../imagenes/motofestbanner.jpg">
+   </div>
+</div>
+<section class="row container">
+   <div class="col m8">
+      <div>
+         <h2 class="section-subheading" style="color: black; font-size: 2rem; letter-spacing: 5px;">
+            <img height="34px" src="../imagenes/chulos.jpeg" alt="">
+            Vive tu moto, <br> Vive tu territorio.
+         </h2>
+         <hr style="height: 2px; background-color: red;">
+         <div class="col m12 text-section textosparrafos" style="margin-top: 0px;">
+            <p>
+              Sé parte del festival de motos más grande de Colombia, todas las pasiones de las motos reunidas en un solo lugar. Un festival lleno de emoción, velocidad, aventura, deporte y adrenalina en el que toda la familia motera se convertirá en protagonista.
+
+<br><br>  Ven con tus amigos y familia a vivir la magia del motociclismo.  ¡Vive con el alma tu mayor pasión!
+  Inscríbete con anticipación en alguna de las actividades diseñadas para tu territorio y podrás disfrutar del Motofest IV como no lo has hecho antes, ingresando al interior del evento con tu moto.
+            </p>
+         </div>
+      </div>
+   </div>
+</section>
+<div class="parallax-container slider-top3">
+<div class="parallax">
+   <img class="imgenparallax" width="100%" src="../imagenes/jackfield.jpg">
+</div></div>
+
+<div class="row">
+
+  <div style="background-color: #B2B2B2; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+
+  </div>
+
+<div style="padding: 0; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+<img style="width: 100%;" src="../imagenes/Testdrives-BN.jpg" alt="">
+</div>
+<div style="padding: 0; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+<img style="width: 100%;" src="../imagenes/Motard-wheelie_White.jpg" alt="">
+</div>
+<div style="background-color: #FF0000; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+
+</div>
+
+<div style="background-color: #231F20; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+
+</div>
+<div style="padding: 0; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+<img style="width: 100%;" src="../imagenes/maxitrail-ok_Red.jpg" alt="">
+</div>
+<div style="padding: 0; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+<img style="width: 100%;" src="../imagenes/endurocross-Black.jpg" alt="">
+</div>
+<div style="background-color: #B2B2B2; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+
+</div>
+<div style="background-color: #FF0000; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+
+</div>
+
+<div style="padding: 0; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+<img style="width: 100%;" src="../imagenes/Clasicas_BN.jpg" alt="">
+</div>
+<div style="padding: 0; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+<img style="width: 100%;" src="../imagenes/Charlas-BN.jpg" alt="">
+</div>
+<div style="background-color: #231F20; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+
+</div>
+<div style="background-color: #B2B2B2; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+
+</div>
+<div style="padding: 0; height: 350px; margin-top: 4px; overflow: hidden;" class="col m6">
+<img style="width: 100%;" src="../imagenes/parchadero_red.jpg" alt="">
+</div>
+
+</div>
+
+
+
+<div class="row container nomargenabajo">
+   <div class="col s6 displaytable altura500 ">
+      <div class="alinearv relative">
+         <div class="absolute2">
+            <span class="paralelogramo2 textosdecorados4">&nbsp;¿Más información?</span><br>
+            <span class="paralelogramo2 textosdecorados5">&nbsp;Escríbenos</span>
+         </div>
+         <img class="imagenautodromo2" src="../imagenes/ChulosRideproday.png" alt="">
+      </div>
+   </div>
+   <form class="col s6 displaytable altura500 ">
+      <div class="alinearv">
+         <div class="row nomargenabajo">
+            <div class="input-field col s12">
+               <img class="prefix prefix2" src="../imagenes/cascos2.png" alt="">
+               <input id="icon_prefix" type="text" class="validate">
+               <label class="vener2" for="icon_prefix">Nombre</label>
+            </div>
+            <div class="input-field col s12">
+               <i class="material-icons prefix ">mail_outline</i>
+               <input id="email" type="email" class="validate">
+               <label class="vener2" for="email">Email</label>
+            </div>
+            <div class="input-field col s12">
+               <img class="prefix prefix3" src="../imagenes/banderas2.png" alt="">
+               <textarea id="textarea1" class="materialize-textarea"></textarea>
+               <label class="vener2" for="textarea1">¿Cómo podemos ayudarte?</label>
+            </div>
+         </div>
+         <div class="paralelogramo tamañonormal botonformulario">
+            <img class="navbar-item-img" src="../imagenes/RP.png" alt="">
+            <a class="black-text">Enviar</a>
+         </div>
+   </form>
+   </div>
+</div>
+<!-- * END Content -->
+<!-- Incluir Contacto -->
+<?php include("secciones/contacto.php"); ?>
+<!-- Aqui el footer -->
+<?php include("secciones/footer.php"); ?>
